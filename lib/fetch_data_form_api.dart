b9b09211f231a2f574820159c99870144ce81019
Awaizkazi <Owaizkazi789@gmail.com>
2024-07-05 00:58:14 +0530

    Caliing API

---
+++ lib/fetch_data_form_api.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FetchDataFromApi extends StatefulWidget {
+  const FetchDataFromApi({super.key});
+
+  @override
+  State<FetchDataFromApi> createState() => _FetchDataFromApiState();
+}
+
+class _FetchDataFromApiState extends State<FetchDataFromApi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:api_fetch_practice/fetch_data_form_api.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(centerTitle: true),
+        body: const FetchDataFromApi(),
+      ),
+    );
+  }
+}
