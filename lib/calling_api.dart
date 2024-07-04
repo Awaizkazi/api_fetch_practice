1fe7e97880421a750c7da6ca6ce0e308caaf7caa
Awaizkazi <Owaizkazi789@gmail.com>
2024-07-05 00:58:30 +0530

    initial commit

---
+++ lib/calling_api.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CallingApi extends StatefulWidget {
+  const CallingApi({super.key});
+
+  @override
+  State<CallingApi> createState() => _CallingApiState();
+}
+
+class _CallingApiState extends State<CallingApi> {
+  @override
+  Widget build(BuildContext context) {
+    return  ();
+  }
+}
--- lib/fetch_data_form_api.dart
+++ lib/fetch_data_form_api.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+import 'dart:convert';
+
+import 'package:api_fetch_practice/model.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class FetchDataFromApi extends StatefulWidget {
   const FetchDataFromApi({super.key});
@@ -8,8 +13,49 @@ class FetchDataFromApi extends StatefulWidget {
 }
 
 class _FetchDataFromApiState extends State<FetchDataFromApi> {
+  List<SamplePosts> samplePosts =
+      []; //TODO Server jo data aaega usko hm is List me store karne wale hai
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemCount: 1,
+      itemBuilder: (context, index) {
+        return Container(
+          height: 120,
+          color: Colors.greenAccent,
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          margin: EdgeInsets.all(10),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('userId : data', style: TextStyle(fontSize: 17)),
+              Text('id : data', style: TextStyle(fontSize: 17)),
+              Text('title : data', style: TextStyle(fontSize: 17)),
+              Text('body : data', style: TextStyle(fontSize: 17)),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<List<SamplePosts>> getData() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+    var data = jsonDecode(response.body.toString());
+
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> index in data) {
+        samplePosts.add(
+          SamplePosts.fromJson(index),
+        );
+      }
+      return samplePosts;
+    } else {
+      samplePosts;
+    }
   }
 }
